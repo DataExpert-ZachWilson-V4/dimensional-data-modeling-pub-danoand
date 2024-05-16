f39c58d5d1d20efa514be7b00361c2efb208eb7e
danoand <danandersonmobile@yahoo.com>
2024-05-15 19:50:12 -0500

    Update query_1.sql
    initial load from old repo

---
+++ submission/query_1.sql
@@ -0,0 +1,22 @@
+-- DDL code to stand up 'actor_films' as a Trino powered table
+CREATE TABLE danfanderson48529.actors (
+  actor VARCHAR,
+  actor_id VARCHAR,
+  -- films is an array of ROWs describing the actor's films for the year
+  films ARRAY(
+    ROW(
+      film VARCHAR,
+      votes INTEGER,
+      rating DOUBLE,
+      film_id VARCHAR
+    )
+  ),
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
